593e0842a8dcd9799b838f072102f41cca254b58
Ocean <dvsocean@icloud.com>
2018-09-18 12:26:10 -0700

    added POST method

--- src/test/java/com/wiremock/wiremock/VerifyReturnDataFromMockServer.java
+++ src/test/java/com/wiremock/wiremock/VerifyReturnDataFromMockServer.java
@@ -2,6 +2,7 @@ package com.wiremock.wiremock;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static org.junit.Assert.assertNotNull;
@@ -10,10 +11,13 @@ import static org.junit.Assert.assertTrue;
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import org.junit.Rule;
 import org.junit.Test;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 public class VerifyReturnDataFromMockServer {
+
+  private RestTemplate restTemplate = new RestTemplate();
 
   //Specifies port for mock server
   @Rule
@@ -21,7 +25,7 @@ public class VerifyReturnDataFromMockServer {
 
 
   @Test
-  public void shouldReturnValueFromMockServer() {
+  public void shouldReturnValueFromMockServerGET_METHOD() {
     //starts mock server
     wireMockRule.start();
 
@@ -33,7 +37,6 @@ public class VerifyReturnDataFromMockServer {
             .withBody("{\"name\":\"Alexander\"}")));
 
     //2. Make a request
-    RestTemplate restTemplate = new RestTemplate();
     String resourceUrl = "http://localhost:5050/flyTeam";
     ResponseEntity<String> response = restTemplate.getForEntity(resourceUrl, String.class);
 
@@ -46,4 +49,27 @@ public class VerifyReturnDataFromMockServer {
     wireMockRule.stop();
   }
 
-}
+  @Test
+  public void shouldReturnValuePOST_METHOD() {
+    wireMockRule.start();
+
+    //1. Stub endpoint
+    stubFor(post(urlEqualTo("/bruceWayne"))
+      .willReturn(aResponse()
+      .withStatus(200)
+      .withHeader("Content-Type", "application/json")
+      .withBody("{\"model\":\"F550\"}")));
+
+    //2. make a request
+    HttpEntity<String> req = new HttpEntity<>("");
+    ResponseEntity<String> res = restTemplate.postForEntity("http://localhost:5050/bruceWayne", req, String.class);
+
+    //3. verify
+    assertNotNull(res);
+    assertTrue(res.getStatusCode().is2xxSuccessful());
+    assertTrue(res.getBody().contains("F5"));
+
+    wireMockRule.stop();
+  }
+
+}//End of class
